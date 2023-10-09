4f42ea33a6ac277ff16c46d79b79179c6198637f
Mustafa Abu Wardeh <64049602+mustafawardeh@users.noreply.github.com>
2023-10-09 19:22:53 +0300

    final++

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,59 +1,56 @@
-import { Link } from 'react-router-dom'
-import { cartImg, githubLogo, logoDark } from '../../assets'
-import './Header.css'
-import { useSelector } from 'react-redux'
-import { useState } from 'react'
+import { Link } from 'react-router-dom';
+import { cartImg, githubLogo, logoDark } from '../../assets';
+import './Header.css';
+import { useSelector } from 'react-redux';
+import { useState } from 'react';
+
 function Header() {
+  const product = useSelector(state => state.bazar.productData);
+  const user = useSelector(state => state.bazar.userInfo);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const product = useSelector(state => state.bazar.productData)
-  const user = useSelector(state => state.bazar.userInfo)
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-  const [MenuState, SetMenuState] = useState('hide')
-  const [HideMenuButton, SetHideMenuButton] = useState('show')
-  const menuhandler = () => {
-    document.getElementById('menuBtn').style.display = 'none'
-
-    document.getElementById('header').style.display = 'block'
-    document.getElementById('closeBtn').style.display = 'block'
-  }
-
-  const closemenu = () => {
-    document.getElementById('menuBtn').style.display = 'block'
-
-    document.getElementById('header').style.display = 'none'
-    document.getElementById('closeBtn').style.display = 'none'
-  }
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
 
   return (
     <div className='relative'>
-      <div className=' cintainer-header flex align-center gap-30'>
-        <div className="header flex align-center justify-between ">
+      <div className='cintainer-header flex align-center gap-30'>
+        <div className="header flex align-center justify-between">
           <Link to={'/'}>
             <div className="logo">
               <img src={logoDark} alt="image" />
             </div>
           </Link>
-          <div id='menuBtn' className={` menu-icon`}>
-            <i onClick={menuhandler} className="fas fa-bars font-l pointer"></i>
+          <div id='menuBtn' className={`${isMenuOpen ? 'hide' : 'menu-icon'} `} onClick={toggleMenu}>
+            <i className="fas fa-bars font-l pointer"></i>
           </div>
-          <div id='header' className={` header-content flex gap-40 align-center`}>
+
+          <div id='header' className={`header-content flex gap-40 align-center ${isMenuOpen === true ? 'mobile' : 'hide'}`}>
+            <p id='closeBtn' onClick={() => closeMenu()} className={` x-close`}>
+              <i className="x-close-icon fa-solid fa-xmark"></i>
+            </p>
             <ul className='header-list list-none flex align-center gap-20 font-m'>
               <Link className='link-home flex justify-center' to={'/'}>
-                <li onClick={() => closemenu()} className='header-list-item home-first font-500 text-decoration color-black'>Home</li>
+                <li onClick={() => closeMenu()} className='header-list-item home-first font-500 text-decoration color-black'>Home</li>
               </Link>
               <Link className='link-home flex justify-center' to={'/'}>
-                <li onClick={() => closemenu()} className='header-list-item font-500 text-decoration color-black'>Pages</li>
+                <li onClick={() => closeMenu()} className='header-list-item font-500 text-decoration color-black'>Pages</li>
               </Link>
               <Link className='link-home flex justify-center' to={'/'}>
-                <li onClick={() => closemenu()} className='header-list-item font-500 text-decoration color-black'>Shop</li>
-              </Link>
-              <Link className='link-home flex justify-center' to={'/'}>
-
-                <li onClick={() => closemenu()} className='header-list-item font-500 text-decoration color-black'>Element</li>
+                <li onClick={() => closeMenu()} className='header-list-item font-500 text-decoration color-black'>Shop</li>
               </Link>
               <Link className='link-home flex justify-center' to={'/'}>
 
-                <li onClick={() => closemenu()} className='header-list-item font-500 text-decoration color-black'>Blog</li>
+                <li onClick={() => closeMenu()} className='header-list-item font-500 text-decoration color-black'>Element</li>
+              </Link>
+              <Link className='link-home flex justify-center' to={'/'}>
+
+                <li onClick={() => closeMenu()} className='header-list-item font-500 text-decoration color-black'>Blog</li>
               </Link>
               <Link to={'/cart'}>
                 <div className='header-image cart font-m'>
@@ -62,20 +59,19 @@ function Header() {
                 </div>
               </Link>
             </ul>
-            <p id='closeBtn' onClick={() => closemenu()} className='x-close'><i className="fa-solid fa-xmark"></i></p>
 
             <Link to={'/login'}>
               {user ?
                 <div className=' flex justify-center'>
-                  <div className='user-info flex gap-10 align-center' onClick={() => closemenu()}>
+                  <div className='user-info flex gap-10 align-center' onClick={() => closeMenu()}>
                     <img className='user-image' src={user.image} />
                     <p className='userName' >{user.name}</p>
                   </div>
                 </div>
                 :
-                <div className='user-info flex gap-10 align-center' onClick={() => closemenu()}>
+                <div className='user-info flex gap-10 align-center' onClick={() => closeMenu()}>
                   <div className='user-icon'>
-                    <i className='fa-solid fa-user ' onClick={() => closemenu()} ></i>
+                    <i className='fa-solid fa-user user-i-icon' onClick={() => closeMenu()} ></i>
                   </div>
                   <p className='userName' >Login Please</p>
                 </div>
